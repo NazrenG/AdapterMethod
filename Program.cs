0585aa33ac628948f6f214aaaab338ba40c12030
NazrenG <nezrin.qu@gmail.com>
2023-11-29 20:45:08 +0400

    adapter method

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Adapter
+{
+    public interface Rectangle
+    {
+        public void setLength(int length);
+        public void setWidth(int width);
+        public void printAreaOfRectangle();
+    }
+
+    public class TennisCourt : Rectangle
+    {
+        public int length;
+        public int width;
+
+        public void setLength(int lenth) { this.length = lenth; }
+        public void setWidth(int width) { this.width = width; }
+        public void printAreaOfRectangle()
+        { //do work
+        }
+    }
+
+    public interface Square
+    {
+        public void setSide(int side);
+        public void printAreaOfSquare();
+    }
+
+    public class Chessboard : Square
+    {
+        public int setLength;
+
+        public void setSide(int side) { this.setLength = side; }
+        public void printAreaOfSquare()
+        { //do work 
+        }
+
+    }
+
+    public  class RectangleAdapter : Square
+    {
+        private Rectangle? rect;
+
+        public RectangleAdapter(Rectangle? rect)
+        {
+            this.rect = rect;
+        }
+
+        public void printAreaOfSquare()
+        { 
+            rect.printAreaOfRectangle();
+        }
+
+        public void setSide(int side)
+        {
+            rect.setWidth(side);
+            rect.setLength(side);
+        }
+    }
+
+ 
+
+}
